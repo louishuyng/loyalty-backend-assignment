369f1f9721b3dbd2753864deb0ea17d4122b0071
Louis <huynguyennbk@gmail.com>
2022-09-10 10:58:57 +0700

    chore: change column username to user_name of user table

--- db/schema.rb
+++ db/schema.rb
@@ -75,11 +75,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_161835) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
+    t.string "user_name", null: false
     t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "transactions", "users"
